ae02c68df6f26ff24b008ca047ae7750eeecedac
Boris Kolpackov <boris@codesynthesis.com>
2016-01-27 12:27:54 +0200

    Add support for parsing cpu-vendor-os target triplets

---
+++ butl/triplet.cxx
@@ -0,0 +1,121 @@
+// file      : butl/triplet.cxx -*- C++ -*-
+// copyright : Copyright (c) 2014-2016 Code Synthesis Ltd
+// license   : MIT; see accompanying LICENSE file
+
+#include <butl/triplet>
+
+#include <stdexcept> // invalid_argument
+
+using namespace std;
+
+namespace butl
+{
+  triplet::
+  triplet (const string& s, string* c)
+  {
+    auto bad = [](const char* m) {throw invalid_argument (m);};
+
+    // Find the first and the last components. The first is CPU and the last is
+    // (part of) SYSTEM, that we know for sure.
+    //
+    string::size_type f (s.find ('-')), l (s.rfind ('-'));
+
+    if (f == 0 || f == string::npos)
+      bad ("missing cpu");
+
+    cpu.assign (s, 0, f);
+
+    if (c != nullptr)
+      *c = cpu;
+
+    // If we have something in between, then the first component after CPU is
+    // VENDOR. Unless it is a first component of two-component system, as in
+    // i686-linux-gnu.
+    //
+    if (f != l)
+    {
+      // [f, p) is VENDOR.
+      //
+      string::size_type p (s.find ('-', ++f)), n (p - f);
+
+      if (n == 0)
+        bad ("empty vendor");
+
+      // Do we have all four components? If so, then we don't need to do any
+      // special recognition of two-component systems.
+      //
+      if (l != p)
+      {
+        l = s.rfind ('-', --l);
+
+        if (l != p)
+          bad ("too many components");
+
+        // Handle the none-* case here.
+        //
+        if (s.compare (l + 1, 5, "none-") == 0)
+          l += 5;
+      }
+      else
+      {
+        // See if this is one of the well-known non-vendors.
+        //
+        if (s.compare (f, n, "linux") == 0 ||
+            s.compare (f, n, "kfreebsd") == 0)
+        {
+          l = f - 1;
+          n = 0; // No VENDOR.
+        }
+      }
+
+      // Handle special VENDOR values.
+      //
+      if (n != 0)
+      {
+        if (s.compare (f, n, "pc") != 0 &&
+            s.compare (f, n, "none") != 0 &&
+            s.compare (f, n, "unknown") != 0)
+        {
+          vendor.assign (s, f, n);
+
+          if (c != nullptr)
+          {
+            *c += '-';
+            *c += vendor;
+          }
+        }
+      }
+    }
+
+    // (l, npos) is SYSTEM
+    //
+    system.assign (s, ++l, string::npos);
+
+    if (system.empty ())
+      bad ("missing os/kernel/abi");
+
+    if (system.front () == '-' || system.back () == '-')
+      bad ("invalid os/kernel/abi");
+
+    if (c != nullptr)
+    {
+      *c += '-';
+      *c += system;
+    }
+
+    // Finally, extract VERSION for some recognized systems.
+    //
+    string::size_type v (0);
+    if (system.compare (0, (v = 6), "darwin") == 0  ||
+        system.compare (0, (v = 7), "freebsd") == 0 ||
+        system.compare (0, (v = 7), "openbsd") == 0 ||
+        system.compare (0, (v = 7), "netbsd") == 0  ||
+        system.compare (0, (v = 7), "solaris") == 0 ||
+        system.compare (0, (v = 7), "aix") == 0     ||
+        system.compare (0, (v = 7), "hpux") == 0)
+    {
+      version.assign (system, v, string::npos);
+      system.resize (system.size () - version.size ());
+    }
+  }
+}
---
+++ tests/triplet/driver.cxx
@@ -0,0 +1,154 @@
+// file      : tests/triplet/driver.cxx -*- C++ -*-
+// copyright : Copyright (c) 2014-2016 Code Synthesis Ltd
+// license   : MIT; see accompanying LICENSE file
+
+#include <cassert>
+#include <iostream>
+#include <stdexcept> // invalid_argument
+
+#include <butl/triplet>
+
+using namespace std;
+using namespace butl;
+
+static bool
+fail (const char*);
+
+static bool
+test (const char*,
+      const char* canon,
+      const char* cpu,
+      const char* vendor,
+      const char* system,
+      const char* version);
+
+int
+main ()
+{
+  assert (fail (""));
+  assert (fail ("mingw32"));
+  assert (fail ("-"));
+  assert (fail ("arm-"));
+  assert (fail ("-mingw32"));
+  assert (fail ("a-b-c-d-e"));
+  assert (fail ("arm-pc--"));
+  assert (fail ("arm-pc-linux-"));
+  assert (fail ("arm-pc--gnu"));
+
+  assert (test ("i686-elf",
+                "i686-elf",
+                "i686", "", "elf", ""));
+
+  assert (test ("arm-eabi",
+                "arm-eabi",
+                "arm", "", "eabi", ""));
+
+  assert (test ("arm-none-eabi",
+                "arm-eabi",
+                "arm", "", "eabi", ""));
+
+  assert (test ("arm-none-linux-gnueabi",
+                "arm-linux-gnueabi",
+                "arm", "", "linux-gnueabi", ""));
+
+  assert (test ("arm-softfloat-linux-gnu",
+                "arm-softfloat-linux-gnu",
+                "arm", "softfloat", "linux-gnu", ""));
+
+  assert (test ("i686-pc-mingw32",
+                "i686-mingw32",
+                "i686", "", "mingw32", ""));
+
+  assert (test ("i686-w64-mingw32",
+                "i686-w64-mingw32",
+                "i686", "w64", "mingw32", ""));
+
+  assert (test ("i686-lfs-linux-gnu",
+                "i686-lfs-linux-gnu",
+                "i686", "lfs", "linux-gnu", ""));
+
+  assert (test ("x86_64-unknown-linux-gnu",
+                "x86_64-linux-gnu",
+                "x86_64", "", "linux-gnu", ""));
+
+  assert (test ("x86_64-linux-gnux32",
+                "x86_64-linux-gnux32",
+                "x86_64", "", "linux-gnux32", ""));
+
+  // Removal of none-.
+  //
+  assert (test ("arm-none",
+                "arm-none",
+                "arm", "", "none", ""));
+
+  assert (test ("arm-unknown-none-eabi",
+                "arm-eabi",
+                "arm", "", "eabi", ""));
+
+  // Version extraction.
+  //
+  assert (test ("x86_64-apple-darwin14.5.0",
+                "x86_64-apple-darwin14.5.0",
+                "x86_64", "apple", "darwin", "14.5.0"));
+
+  assert (test ("x86_64-unknown-freebsd10.2",
+                "x86_64-freebsd10.2",
+                "x86_64", "", "freebsd", "10.2"));
+
+  assert (test ("x86_64-pc-openbsd5.6",
+                "x86_64-openbsd5.6",
+                "x86_64", "", "openbsd", "5.6"));
+
+  assert (test ("sparc-sun-solaris2.9",
+                "sparc-sun-solaris2.9",
+                "sparc", "sun", "solaris", "2.9"));
+}
+
+static bool
+test (const char* s,
+      const char* canon,
+      const char* cpu,
+      const char* vendor,
+      const char* system,
+      const char* version)
+{
+  string c;
+  triplet t (s, c);
+
+  auto cmp = [] (const string& a, const char* e, const char* n) -> bool
+    {
+      if (a != e)
+      {
+        cerr << n << " actual: " << a << endl
+             << n << " expect: " << e << endl;
+
+        return false;
+      }
+
+      return true;
+    };
+
+  return
+    cmp (c, canon, "canonical") &&
+    cmp (t.cpu, cpu, "cpu") &&
+    cmp (t.vendor, vendor, "vendor") &&
+    cmp (t.system, system, "system") &&
+    cmp (t.version, version, "version");
+}
+
+static bool
+fail (const char* s)
+{
+  try
+  {
+    triplet t (s);
+    cerr << "nofail: " << s << endl;
+    return false;
+  }
+  catch (invalid_argument& e)
+  {
+    //cerr << e.what () << endl;
+  }
+
+  return true;
+}
