7ca36cc5fc510e63f5caf830433231bbccca622f
Boris Kolpackov <boris@codesynthesis.com>
2016-12-16 17:12:43 +0200

    Fix the previous fix

--- butl/utility.cxx
+++ butl/utility.cxx
@@ -7,16 +7,18 @@
 namespace butl
 {
 #ifndef BUTL_CXX17_UNCAUGHT_EXCEPTIONS
+
 #ifdef BUTL_CXX11_THREAD_LOCAL
   thread_local
 #else
   __thread
 #endif
   bool exception_unwinding_dtor_ = false;
-#endif
 
 #ifdef _WIN32
   bool&
   exception_unwinding_dtor () {return exception_unwinding_dtor_;}
+#endif
+
 #endif
 }
